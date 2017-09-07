efea35093d646137ab7b890e6a59894a1f95b2f6
paulvaldez <valdezpaul00@gmail.com>
2017-09-07 10:40:04 -0500

    Added variables for all the weather elements.

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,19 +1,19 @@
 (function(){
     "use strict";
-    $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-        APPID: "ebfe6fcbf45c478998118b21ec89df74",
-        lat:    29.423017,
-        lon:    -98.48527,
-        units:  "imperial",
-        cnt: 3,
-    }).done(function(weather){
-        var icon = weather.list[1].weather[1].icon;
-        var url = "http://openweathermap.org/img/w/" + icon + ".png"
-            var img = "<img src=''" + url + "'>"
-                $('body').append(img);
-
-        console.log(url);
-    });
+    // $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+    //     APPID: "52520910551c39dc8d3da4d4fcb54c22",
+    //     lat:    29.423017,
+    //     lon:    -98.48527,
+    //     units:  "imperial",
+    //     cnt: 3,
+    // }).done(function(weather){
+    //     var icon = weather.list[1].weather[1].icon;
+    //     var url = "http://openweathermap.org/img/w/" + icon + ".png"
+    //         var img = "<img src=''" + url + "'>"
+    //             $('body').append(img);
+    //
+    //     console.log(url);
+    // });
 
     // var request = $.ajax("http://api.openweathermap.org/data/2.5/weather");
     // request.done(dayOne);
@@ -27,7 +27,27 @@
     //     dayOne();
     //
     // }
+    $.get("http://api.openweathermap.org/data/2.5/forecast", {
+        APPID: "52520910551c39dc8d3da4d4fcb54c22",
+            lat:    29.423017,
+            lon:    -98.48527,
+            units:  "imperial",
+            cnt: 3
+    }).done(function(data) {
+        console.log(data);
+        var html = "<div class=\"box col-md-4\">";
+        var minTemp = data.list[0].main.temp_min + "/";
+        var maxTemp = data.list[0].main.temp_max;
+        var clouds = data.list[0].weather[0].main;
+        var cloudsDescription = data.list[0].weather[0].description;
+        var humidity = data.list[0].main.humidity;
+        var wind = data.list[0].wind.speed;
+        var pressure = data.list[0].main.pressure;
 
+
+
+        $(".col-md-4").append(total);
+    });
 
 
 
